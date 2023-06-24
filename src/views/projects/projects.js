34c0d917716f758dd508409ee8dd341b724c75f6
DunKim <darkgx21@gmail.com>
2023-06-24 18:55:49 +0900

    나만의 포트폴리오 내용 수정

--- src/views/projects/projects.js
+++ src/views/projects/projects.js
@@ -18,6 +18,36 @@ export default{
                 //     IDE: 'Android Studio'
                 // },
                 {
+                    name: '가계부 플랫폼 서버',
+                    img: 'account-book-admin-web',
+                    datePeople:'2023.5.11~2023.06.19(33일)(1인 프로젝트)',
+                    content: '인프런 중에 김영한 강의를 듣고 배웠던 내용을 가계부 플랫폼에다 적용하고 싶었고 Spring boots가 Spring를 편리하게 설정할 수 있고 SOLID 원칙을 잘 지킬 수 있고 QueryDsl 라이브러리를 통해 생산성 있게 개발할 수 있어서 만들게 되었습니다. RDBMS 데이터베이스를 쉽게 연동할 수 있는 JPA를 활용하고 회원에 대한 기능에 유용한 JWT 그리고 Spring Security를 적용했습니다. 왜나하면 로그인할 시 토큰으로 로그인 여부를 확인할 수 있고 bcrypt 암호화 방식을 활용하여 비밀번호를 암호화해서 보안성이 좋아지기 때문입니다. 그리고 실제 서버를 호스팅 하고 싶어서 AWS EC2으로 Spring boots 서버를 배포했습니다.',
+                    source: 'https://github.com/seongchangkim/account-book',
+                    domain: 'http://ec2-3-38-238-225.ap-northeast-2.compute.amazonaws.com:8080/login',
+                    skillStack: 'Spring boots, Java, MySQL',
+                    IDE: 'Intellij'
+                },
+                {
+                    name: '가계부 앱',
+                    img: 'account-book-app',
+                    datePeople:'2023.5.11~2023.06.19(1달)(1인 프로젝트)',
+                    content: 'Flutter가 다른 앱 네이티브 프로그래밍 언어보다 단기간에 Android과 IOS 둘 다 개발할 수 있고 요새 가계부를 작성하고 있기에 그 부분을 서비스화하고 싶어서 가계부 앱을 만들게 되었습니다. 기능은 서버에서 토큰 인증 방식으로 회원을 관리하도록 설정해놓고 로그인, 로그아웃, 회원가입, 소셜 로그인(카카오) 그리고 프로필 기능들을 백엔드 api와 연동하여 구현했습니다, 그리고 가계부 CRUD 기능을 백엔드 api과 연동하여 구현했습니다.',
+                    source: 'https://github.com/seongchangkim/account_book_app.git',
+                    domain: '',
+                    skillStack: 'Flutter, Dart',
+                    IDE: 'Visual Studio Code'
+                },
+                {
+                    name: '가계부 앱 관리자 회원 웹 프로젝트',
+                    img: 'account-book-admin-web',
+                    datePeople:'2023.5.26~2023.06.09, 2023.06.18(1인 프로젝트)',
+                    content: 'Vue.js가 직관적이고 양방향 데이터 바인딩(Angular)과 가상 돔(React)의 특징을 가지고 SPA 프레임워크 중에서 진입장벽이 낮아서 가계부 앱 관리자 회원 관리 웹 프로젝트를 가계부 웹 클라이언트 서비스로 만들게 되었습니다. 기능은 로그인, 로그아웃, 회원 CRUD 그리고 프로필 기능들을 백엔드 api와 연동하여 구현했습니다. 그리고 백엔드에서 페이징 관련 response 값을 받아 회원 목록을 페이징 처리 및 검색을 할 수 있도록 구현했습니다.',
+                    source: 'https://github.com/seongchangkim/account_book_admin_web.git',
+                    domain: '',
+                    skillStack: 'Vite, Vue.js, JavaScript, npm, CSS3, HTML5, tailwindCSS',
+                    IDE: 'Visual Studio Code'
+                },
+                {
                     name: '자산 포트폴리오 관리 앱',
                     img: 'stockprod',
                     datePeople:'2023.1.5~2023.02.17(1인 프로젝트)',
@@ -46,6 +76,16 @@ export default{
                     domain: 'https://seongchangkim.github.io/',
                     skillStack:  'Vue, JavaScript, npm, CSS3, HTML5, bootstrap',
                     IDE: 'Visual Studio Code'
+                },
+                {
+                    name: '웹 드라이브 프로그램',
+                    img: 'web-cloud-program',
+                    datePeople:'2021.7.17~2023.09.08(50일)(1인 프로젝트)',
+                    content: '스프링 프레임워크를 독학하여 공부했던 내용을 바탕으로 Spring framework로 웹 드라이브 프로그램을 만들게 되었습니다. 그것을 만들면서 보안성을 높이기 위해 Spring Security를 적용했고 로그인, 로그아웃, 비밀번호 찾기, 아이디 찾기, 관리자 관련된 기능, 게시판 CRUD, 파일 업로드, 파일 다운로드, 웹 드라이브 휴지통 기능 등을 구현했습니다.',
+                    source: 'https://github.com/seongchangkim/SpringFrameworkBootCloud1-SpringFramework.git',
+                    domain: '',
+                    skillStack: 'HTML5, CSS3, JavaScript, Bootstrap, jQuery, Java, JSP, MySQL, Spring Framework',                    
+                    IDE: 'Eclipse'
                 },
                 {
                     name: '한 일 목록',
--- src/views/skills/skills.js
+++ src/views/skills/skills.js
@@ -4,13 +4,13 @@ import Title from "@/components/title/title.vue";
 export default{
     data(){
         return{
-            frontEndArr: [["html5", 0], ["css3", 0], ["js", 0], ["vuejs", 0], ["reactjs", 0],["bootstrap", 0]],
-            backEndArr: [["nodejs", 500]],
-            rdbmsArr: [["mysql", 0]],
-            IDEArr: [["vscode", 0], ["webstrom", 0]],
-            appDevelopmentArr: [["flutter", 0]],
-            versionControlArr: [["git", 0], ["github", 0]],
-            certificateArr: [["certficate", 0]],
+            frontEndArr: [["html5", 0, 0], ["css3", 0, 0], ["js", 0, 0], ["vuejs", 0, 0], ["reactjs", 0, 0],["bootstrap", 0, 0], ["tailwindcss", 450, 100]],
+            backEndArr: [["nodejs", 500, 0], ["springBoot", 0, 0]],
+            rdbmsArr: [["mysql", 0, 0]],
+            IDEArr: [["vscode", 0, 0], ["webstrom", 0, 0], ["IntelliJ-IDEA", 350, 0]],
+            appDevelopmentArr: [["flutter", 0, 0]],
+            versionControlArr: [["git", 0, 0], ["github", 0, 0]],
+            certificateArr: [["certficate", 0, 0]],
             skills: "SKILL STACK"
         }
     },
